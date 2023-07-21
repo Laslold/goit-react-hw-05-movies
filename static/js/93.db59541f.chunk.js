839659b73230d6295c066e73ed95129a640bd452
Laslold <116965660+Laslold@users.noreply.github.com>
2023-07-21 09:33:03 +0000

    Deploying to gh-pages from @ Laslold/goit-react-hw-05-movies@642d0f96b0068c046c970b32d6a81be38c07e43e 🚀

--- static/js/93.db59541f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[93],{93:function(e,n,o){o.r(n),o.d(n,{default:function(){return a}});o(791);var r=o(87),t=o(184),a=function(){return(0,t.jsxs)("main",{children:[(0,t.jsx)("h2",{children:"Page not found"}),(0,t.jsx)("p",{children:"The page you are looking for does not exist."}),(0,t.jsx)(r.rU,{to:"/",children:"To home page"})]})}}}]);
-//# sourceMappingURL=93.db59541f.chunk.js.map
